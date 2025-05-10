e57e7e34ef77ec50c781e2be80724a019913a243
Tvrtko <tvrtkobabic2@gmail.com>
2025-05-10 19:45:32 +0200

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://tvrtk0.github.io",
-  base: "/apartment-smile",
+  site: "https://www.apartmentsmile.com",
 });
